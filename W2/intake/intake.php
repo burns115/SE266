133f9676d1b5bd1ddf3b6df88d1d4ad1b0668e8b
burns115 <rburns@email.neit.edu>
2022-12-08 13:49:20 -0500

    Adding New Stuff :>

--- W2/intake/intake.php
+++ W2/intake/intake.php
@@ -144,14 +144,14 @@
 <body>
 
     <div class="navbar">
-        <a href="mainpage.php">Home</a>
+        <a href="../mainpage/mainpage.php">Home</a>
             <div class="dropdown">
                 <button class="dropbtn" onclick="dropDown()">Assignments
                     <i class="fa fa-caret-down"></i>
                 </button>
                 <div class="dropdown-content" id="myDropdown">
                     <a href="../W1/index.php" target="_blank">Week 1 - Fizz Buzz</a>
-                    <a href="mainpage.php">Week 2 - Main Course Page</a>
+                    <a href="../mainpage/mainpage.php">Week 2 - Main Course Page</a>
                     <a href="intake.php" target="_blank">Week 2 - Patient Intake Form</a>
                     <a href="">Week 3 - None</a>
                     <a href="">Week 4 - None</a>
@@ -163,9 +163,9 @@
                     <a href="">Week 10 - None</a>
                 </div>
             </div>  
-        <a href="php_resources.php">PHP Resources</a>
-        <a href="git_resources.php">Git Resources</a>
-        <a href="hobbies.php">Hobbies</a>
+        <a href="../mainpage/php_resources.php">PHP Resources</a>
+        <a href="../mainpage/git_resources.php">Git Resources</a>
+        <a href="../mainpage/hobbies.php">Hobbies</a>
         <a href="https://github.com/burns115/SE266" target="_blank">Ryan's GitHub Repo</a>
     </div>
 
